7513a483bdbcbe726f90056978acb229d249ef3d
bo <764134507@qq.com>
2021-10-24 21:55:35 +0800

    批量撤销本地修改

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("123");
 		int a = 1;
-		int b = 2;
+		String b = "world"; 
 
 	}
 
--- src/GitTest1.java
+++ src/GitTest1.java
@@ -3,6 +3,7 @@ public class GitTest1 {
 
 	public static void main(String[] args) {
 		System.out.println("wo");
+		int c = 3; 
 
 	}
 
